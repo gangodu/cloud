d58d6a3b85978f4b9b6983d9b86d99048eb290bb
Praveen Gangodu <gangodu@linux.com>
2017-04-24 15:02:54 -0700

    Something to run a map reduce client job on a Hadoop cluster on AWS

---
+++ src/main/java/awsREST/Client.java
@@ -0,0 +1,35 @@
+package awsREST;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+/**
+ * Created by praveen on 1/8/17.
+ */
+public class Client {
+
+    public String handler(int input, Context context) {
+        LambdaLogger logger = context.getLogger();
+        StringBuilder result = new StringBuilder("");
+        result.append("Input Parameter " + input + " is ");
+
+        if (isPrime(input)) {
+            result.append("a PRIME");
+        } else {
+            result.append("NOT a PRIME");
+        }
+        logger.log(result.toString());
+        return result.toString();
+    }
+
+    private boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        long sqrtN = (long) Math.sqrt(n) + 1;
+        for (long i = 6L; i <= sqrtN; i += 6) {
+            if (n % (i - 1) == 0 || n % (i + 1) == 0) return false;
+        }
+        return true;
+    }
+}
